36748e19568ff8c8bba89fd6e439ef3e18eec7e5
David Winter <dwinte3@students.towson.edu>
2017-03-13 19:17:22 -0400

    Task 2: Auditing Code
    This commit includes the audits made to high severity violations as
    assessed by the CodePro tool. Please take note of the
    CodeProAuditResults, RefactoringsAuditResults, and the
    SecurityAuditResults html files in the reqs folder of the program.

--- CoffeeMaker/src/edu/towson/cis/cosc442/project4/coffeemaker/CoffeeMaker.java
+++ CoffeeMaker/src/edu/towson/cis/cosc442/project4/coffeemaker/CoffeeMaker.java
@@ -72,7 +72,8 @@ public class CoffeeMaker {
         if(r != null) {
 	        for(int i = 0; i < NUM_RECIPES; i++) {
 	            if(r.equals(recipeArray[i])) {
-	                recipeArray[i] = recipeArray[i]; 
+	               //What does this do? Probably missing a this reference
+	            	//this.recipeArray[i] = recipeArray[i];
 	                canDeleteRecipe = true;
 	            }
 	        }
